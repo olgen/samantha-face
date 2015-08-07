e47fa99cc37349d2d1b70b0d3f2e0a277af4e463
Eugen Martin <eugeniusmartinus@gmail.com>
2015-08-07 13:07:58 +0200

    add webpack.config.js, add first hello-world version

---
+++ client/main.jsx
@@ -0,0 +1,7 @@
+// require('./assets/app.css');
+// require('./assets/menu.css');
+
+var React = require('react');
+var App = require('./components/layout/App.jsx');
+
+React.render(<App/>, document.body);
