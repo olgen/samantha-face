81934693aff0b99c60b79fdce32114f84b4522a8
Will Madden <w.a.madden@gmail.com>
2015-08-08 16:28:27 +0200

    Render lanes and items in reasonable-looking rows

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,22 +1,49 @@
 import React from "react";
-import commitsSmall from "json!commits_small.json";
+import commitsWithTopics from "json!commits_with_topics.json";
+
+function mapCommitsToLanes(commits) {
+  return commits.reduce((memo, commit) => {
+    commit.topics.forEach((topic) => {
+      if (memo[topic] === undefined) {
+        memo[topic] = [];
+      }
+      memo[topic].push(commit);
+    });
+    return memo;
+  }, {});
+}
+
+function renderLane({ title, items }) {
+  return (
+    <div className="lane" style={{ width: items.length * 20 }}>
+      <div className="lane-title">{ title }</div>
+      {
+        items.map( (item) => {
+          return (
+            <div className="lane-item">
+              <div className="lane-item-content">{ item.title }</div>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+}
+
+function renderLanes(lanes) {
+  return (
+    <div className="lanes">
+      { Object.keys(lanes).map((name) => renderLane({ title: name, items: lanes[name] })) }
+    </div>
+  );
+}
 
 const Timeline = React.createClass({
   propTypes: {
   },
 
   render() {
-    return (
-      <div>
-        {
-          commitsSmall.map((commit) => {
-            return (
-              <div> { commit.title }, { commit.created_at } </div>
-            );
-          })
-        }
-      </div>
-    );
+    return renderLanes(mapCommitsToLanes(commitsWithTopics));
   },
 });
 
